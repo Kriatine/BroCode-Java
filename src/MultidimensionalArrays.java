78798fd76bcad6291bd76437b691ae95912ab6fd
Kriatine <konyayagiz@gmail.com>
2025-10-21 23:22:41 +0300

    Initial commit

--- src/MultidimensionalArrays.java
+++ src/MultidimensionalArrays.java
@@ -1,2 +1,21 @@
 public class MultidimensionalArrays {
+    public static void main(String[] args){
+
+        String[] fruits = {"apple", "orange", "banana"};
+        String[] vegetables = {"potato", "onion", "carrot"};
+        String[] meats = {"chicken", "pork", "beef", "fish"};
+
+        String [][] groceries = {fruits, vegetables, meats};
+
+        groceries[0][0] ="pineapple";
+        groceries[1][2] ="tomato";
+
+        for(String[] foods : groceries){
+            for(String food : foods){
+                System.out.print(food + " "  );
+            }
+            System.out.println(); // This makes them show sepereate arrays in seperate rows
+        }
+
+    }
 }
--- src/deneme101t.java
+++ src/deneme101t.java
@@ -1,13 +1,9 @@
 import java.util.Scanner;
 
-public class CSE101T_Assignment1YağızKonya {
-    static void main(String[] args){
+public class deneme101t {
+    static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        int hp = 50;
-        int ap = 10;
-        int dp = 5;
 
         System.out.println("--- Welcome to the Digital Monster Creation Arena ---");
         System.out.print("Let's create your first monster. Give it a cool name: ");
@@ -20,19 +16,7 @@ public class CSE101T_Assignment1YağızKonya {
         System.out.println("[3] Earth (Defense-oriented)");
         System.out.print("Your choice(1-3): ");
         String element = scanner.nextLine();
-        if(element.equals("1")) {
-            element = "Fire";
-            ap += 5;
-        }
-        else if(element.equals("2")) {
-            element = "Water";
-            hp += 20;
-        }
-        else if(element.equals("3")) {
-            element = "Earth";
-            dp += 10;
-        }
-        else{
+        if(!element.equals("1") &&!element.equals("2") &&!element.equals("3")){
             System.out.println("ERROR: Invalid element choice. Exiting program.");
             return;
         }
@@ -42,36 +26,52 @@ public class CSE101T_Assignment1YağızKonya {
         System.out.println("[2] Rare (Bonus to all stats)");
         System.out.print("Your choice (1-2): ");
         String potential = scanner.nextLine();
-        if(potential.equals("1"))
-            potential = "Normal";
-        else if(potential.equals("2")){
-            potential = "Rare";
-            hp += 10;
-            ap += 10;
-            dp += 10;
-        }
-        else{
+        if(!potential.equals("1") && !potential.equals("2")){
             System.out.println("ERROR: Invalid potential choice. Exiting program.");
             return;
         }
         System.out.println();
         System.out.print("Finally, enter a number between 1 and 10 to influence its destiny: ");
         int destiny = scanner.nextInt();
-       if(destiny > 10 || destiny < 1){
-           System.out.println("ERROR: Destiny number must be between 1 and 10. Exiting program.");
-           return;
-       }
-       else{
-           if(element.equals("Fire"))
-           ap += destiny;
-           else if(element.equals("Water"))
-               hp += destiny;
-           else
-               dp += destiny;
-
-       }
+        if(destiny < 1 || destiny > 10) {
+            System.out.println("ERROR: Destiny number must be between 1 and 10. Exiting program.");
+            return;
+        }
         System.out.println();
         System.out.println("Excellent choices! Your monster " + name + " is being created...");
+
+        int hp = 50;
+        int ap = 10;
+        int dp = 5;
+
+        if(element.equals("1")) {
+            element = "Fire";
+            ap += 5;
+        }
+        else if(element.equals("2")) {
+            element = "Water";
+            hp += 20;
+        }
+        else{
+            element = "Earth";
+            dp += 10;
+        }
+
+        if(potential.equals("1"))
+            potential = "Normal";
+        else{
+            potential = "Rare";
+            hp += 10;
+            ap += 10;
+            dp += 10;
+        }
+
+        if(element.equals("Fire"))
+            ap += destiny;
+        else if(element.equals("Water"))
+            hp += destiny;
+        else
+            dp += destiny;
 
         System.out.println();
         System.out.println("--- MONSTER PROFILE CARD ---");
@@ -84,8 +84,5 @@ public class CSE101T_Assignment1YağızKonya {
         System.out.println("Defense Power (DP): " + dp);
         System.out.println("---------------------------");
 
-
-        }
-
     }
-
+}
